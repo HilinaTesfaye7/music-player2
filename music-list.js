cc814739467349455189f77997130efef00cda25
Hilina78 <89244358+HilinaTesfaye7@users.noreply.github.com>
2021-09-28 13:10:19 +0300

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,45 @@
+let allMusic =[
+    {
+        name: "Dishtagina",
+        artist:"smn",
+        img:"Dishtagina",
+        src:"Dishtagina"
+
+    },
+    {
+        name: "kasat",
+        artist:"nom",
+        img:"kasat",
+        src:"kasat"
+
+    },
+    {
+        name: "nanye",
+        artist:"nom",
+        img:"nanye",
+        src:"nanye"
+
+    },
+    {
+        name: "tfa milyn",
+        artist:"nom",
+        img:"tfa  milyn",
+        src:"tfa milyn"
+
+    },
+    {
+        name: "thon enda",
+        artist:"nom",
+        img:"thon enda",
+        src:"thon enda"
+
+    },
+    {
+        name: "zora zora",
+        artist:"nom",
+        img:"zora zora",
+        src:"zora zora"
+
+    },
+   
+]
